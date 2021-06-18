57c501e04c79db142cdfb051133771e1bda1e338
estefaneluz <estefanecortes@gmail.com>
2021-06-18 17:20:01 -0300

    filtrar por categoria

--- cubos-checkout/controladores/controllers.js
+++ cubos-checkout/controladores/controllers.js
@@ -3,7 +3,15 @@ const {lerArquivo} = require("../bibliotecaFS")
 
 async function listarProdutos(req, res){
     const {produtos} = await lerArquivo()
-    const produtosEstoque = produtos.filter(produto => produto.estoque > 0)
+    const {categoria, precoInicial, precoFinal} = req.query
+
+    let produtosEstoque = produtos.filter(produto => produto.estoque > 0)
+
+    if(categoria){
+        produtosEstoque = produtosEstoque.filter
+        (produto => produto.categoria.toLowerCase() === categoria.toLowerCase())
+    }
+    
     res.json(produtosEstoque)
 }
 
