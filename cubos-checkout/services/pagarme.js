5efb3d00bbbb0127986f8e18e7207c1b77386594
estefaneluz <estefanecortes@gmail.com>
2021-06-23 17:27:15 -0300

    done

--- cubos-checkout/services/pagarme.js
+++ cubos-checkout/services/pagarme.js
@@ -1,9 +1,10 @@
 const axios = require('axios')
+const api_key = require('../../apikey')
 
 const instanciaAxios = axios.create({
   baseURL: 'https://api.pagar.me/1/',
   params: {
-    api_key: 'ak_test_rFF3WFkcS9DRdBK7Ocw6QOzOOQEScS',
+    api_key: api_key,
   },
 })
 
